78ed51cfe85245c07f44361d26cc8a36be914ae4
Rafael Correa <rafael_correa_3@hotmail.com>
2016-08-16 09:39:35 -0500

    primer ejercicio

---
+++ ejercicio.hs
@@ -0,0 +1,6 @@
+fact 1 = 1
+fact x = x * fact (x-1)
+
+maximo [] = 0
+maximo [x] = x
+maximo(a:b:c) = let mayor(x, y) = if x > y then x else y in if c==[] then mayor(a,b) else maximo([mayor(a,b)] ++ c)
