772874e9212f51d9565c75e93757dafd20ca36ea
Rafael Correa <rafael_correa_3@hotmail.com>
2016-11-01 08:49:17 -0600

    ordenar

--- tarea2.hs
+++
@@ -1,7 +0,0 @@
-raiz x guess 0 = guess
-raiz x guess n = (if n <= 1 then newguess else raiz x newguess (n-1)) where newguess = guess - ((guess*guess - x)/(2*guess))
-
-quicky [] = []
-quicky (x:xs) = let greaterPivot = quicky [a | a <- xs, a > x]
-                    smallerPivot = quicky [a | a <- xs, a <= x]
-                in smallerPivot ++ [x] ++ greaterPivot
